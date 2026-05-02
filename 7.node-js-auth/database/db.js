f8ea197e8b50e9b457a0ae7d08a83032e1463909
Amirsohail335 <amirsohail335@gmail.com>
2026-05-02 20:18:19 +0530

    connect

--- 7.node-js-auth/database/db.js
+++ 7.node-js-auth/database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected Successfully!!!");
+    console.log("MongoDB connect Successfully!!!");
   } catch (error) {
     console.error("MonogDB connect Failed!!!");
     process.exit(1);
